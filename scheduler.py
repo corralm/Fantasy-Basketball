1b24ce69cbeb157bb029a37b8ef2a5c39a1cbf85
Miguel Corral Jr <corralm@users.noreply.github.com>
2021-01-02 14:13:59 -0800

    Add fetch_free_agents script to scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -2,7 +2,9 @@ import os
 from pathlib import PurePath
 # pip install hickory
 
-script = PurePath('.').joinpath('fetch_yahoo.py')
+yahoo = PurePath('.').joinpath('fetch_yahoo.py')
+espn = PurePath('.').joinpath('fetch_free_agents.py')
+scripts = [yahoo, espn]
 
 
 def get_fetch_times():
@@ -21,16 +23,24 @@ def get_fetch_times():
     return times.rstrip(',')
 
 
-def schedule_script():
+def schedule_fetch_yahoo():
     """Schedules 'fetch_yahoo' script using Hickory."""
     times = get_fetch_times()
-    os.system(f"hickory schedule {script} --every={times}")
+    os.system(f"hickory schedule {yahoo} --every={times}")
 
 
-def kill_script():
+def schedule_fetch_free_agents():
+    """Schedules 'fetch_free_agents' script using Hickory."""
+    times = '@7am,5pm'
+    os.system(f"hickory schedule {espn} --every={times}")
+
+
+def kill_scripts():
     """Kills 'fetch_yahoo' script using Hickory."""
-    os.system(f"hickory kill {script}")
+    for s in scripts:
+        os.system(f"hickory kill {s}")
 
 
 if __name__ == '__main__':
-    schedule_script()
+    schedule_fetch_yahoo()
+    schedule_fetch_free_agents()
